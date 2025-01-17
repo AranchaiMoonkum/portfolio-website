c33bdd1a3a7ed5a6453477ea1e3d9293b373169f
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2025-01-17 17:41:20 +0700

    changed header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,9 +4,8 @@ export default function Header() {
     return (
         <header className="flex flex-col gap-1">
             <h1 className="inline font-bold">Aranchai Moonkum</h1>
-            <h2 className="flex flex-row gap-3">
-                <span>A Software Engineering Student at</span>
-                <Link href="https://www.up.ac.th/">University of Phayao</Link>
+            <h2>
+                A Software Engineering Student at <Link href="https://www.up.ac.th/">University of Phayao</Link>
             </h2>
         </header>
     )
