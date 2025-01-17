8b660c862fc79e93fe6d4324578deabe949582c6
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2025-01-17 17:40:55 +0700

    added content on index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link"
+
 export default function Home() {
     return (
-        <></>
+        <article className="flex flex-col gap-12">
+            <span>
+                <Link href="/works/pos-system" className="relative inline-flex flex-col gap-1 no-underline">
+                    <h1>POS system <span className="text-red-500">[Work in process]</span></h1>
+                    <span className="text-neutral-500">Jan 2025 - Current</span>
+                </Link>
+            </span>
+            <span>
+                <Link href="/works/spark-up" className="relative inline-flex flex-col gap-1 no-underline">
+                    <h1>Spark UP</h1>
+                    <span className="text-neutral-500">July 2024 - Oct 2024</span>
+                </Link>
+            </span>
+            <span>
+                <Link href="/works/stage" className="relative inline-flex flex-col gap-1 no-underline">
+                    <h1>Stage</h1>
+                    <span className="text-neutral-500">Nov 2023 - Mar 2024</span>
+                </Link>
+            </span>
+        </article>
     )
 }
