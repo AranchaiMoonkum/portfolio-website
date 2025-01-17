2152f84b1109e43e05a690aee2f3c301aaee0a31
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2025-01-17 16:32:51 +0700

    created navbar.tsx

---
+++ src/components/navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <ul className="flex flex-row gap-4">
+                <li>
+                    <Link href="/about" className="text-neutral-500">About</Link>
+                </li>
+                <li>
+                    <Link href="/connect" className="text-neutral-500">Connect</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
