c692a3aeeb1306b4888c38edd786bcc94666c6f3
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2025-01-17 17:42:24 +0700

    created about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+export default function About() {
+    return (
+        <article className="flex flex-col gap-12">
+            <h1 className="font-bold">About</h1>
+            <p>
+                I am a student currently studying Software Engineering at the University of Phayao, and I am in my second year of study.
+            </p>
+            <p>
+                I am interested in web development and have experience in using React, Vue,  Next.js, Nuxt.js, TypeScript, Prisma, and Tailwind CSS. And I am currently focusing
+                on Back-end development with tools like Supabase, NextAuth.js, and Prisma by using API routes in Next.js.
+            </p>
+            <p>
+                I am passionate about learning new technologies and programming languages, and I am currently challenging myself to continuously grow and excel in my career.
+            </p>
+        </article>
+    )
+}
